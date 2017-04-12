e5d961bca02e519d6fb5f787feacedb144073065
Andrei Dziubiankou <dziubiankou@gmail.com>
2017-04-12 14:29:13 +0300

    fixed, comments delete

--- task_3_binarySearch/binarySearch.js
+++ task_3_binarySearch/binarySearch.js
@@ -21,28 +21,7 @@ console.log('binarySearch([' + array + '], 19): ' + binarySearchWhile(array, 19)
 console.log('binarySearch([], 0): ' + binarySearch([], 0));
 ;
 function binarySearch(array, key, start, end){
-<<<<<<< HEAD
-  if (end < start){
-=======
-  //with while
-  /*var low = 0;
-    var high = array.length - 1;
-    while (low <= high){
-      var mid = parseInt((low + high) / 2);
-      var midValue = array[mid];
-
-      if (midValue < key)
-          low = mid + 1;
-      else if (midValue > key)
-          high = mid - 1;
-      else
-          return mid;
-    }
-    return -1; */
-
-  //with recursion
    if (end < start){
->>>>>>> bcb1d286a1f34480d850c9246425c6804fd285e0
     return -1
   }
   var low = start || 0;
@@ -56,26 +35,6 @@ function binarySearch(array, key, start, end){
     } else {
         return binarySearch(array, key, mid + 1, high);
     }
-<<<<<<< HEAD
-=======
-}
-
-function binarySearchWhile(array, key){
-  var low = 0;
-  var high = array.length - 1;
-  while (low <= high){
-    var mid = parseInt((low + high) / 2);
-    var midValue = array[mid];
-
-    if (midValue < key)
-        low = mid + 1;
-    else if (midValue > key)
-        high = mid - 1;
-    else
-        return mid;
-  }
-  return -1;
->>>>>>> bcb1d286a1f34480d850c9246425c6804fd285e0
 }
 
 function binarySearchWhile(array, key){
